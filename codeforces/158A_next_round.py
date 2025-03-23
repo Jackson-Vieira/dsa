30326865cc4e25dd8f2bb25f9e58623b194acb24
Jackson Vieira <jacksonvieiira12@gmail.com>
2025-03-23 18:02:10 -0300

    refactor solution for Codeforces problem 158A: Next Round to improve logic for counting qualifying participants

--- codeforces/158A_next_round.py
+++ codeforces/158A_next_round.py
@@ -1,2 +1,3 @@
 n, k = input().split(" ")
-print(len([i for i in input().split(" ") if int(i) >= int(k)]))
+ps = input().split(" ")
+print(len([i for i in ps if int(i) >= int(ps[int(k)-1]) and int(i) > 0]))
